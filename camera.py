469a864b4474ceefe8c61dbc938451af38f0fcab
sumitmane1 <108738719+sumitmane1@users.noreply.github.com>
2022-08-23 23:28:11 +0530

    Update camera.py

--- camera.py
+++ camera.py
@@ -1,21 +1,25 @@
+from ast import excepthandler
 from multiprocessing import connection
 import pandas as pd
 import cv2
-df = pd.read_csv(r"C:\Users\Sumit\Desktop\CV RTSP Task\test.csv")
+df = pd.read_csv("test.csv")
 Rtps_url = []
 Store = int(input("Enter Store Number:"))
 for i,row in df.iterrows():
     if row['store']==Store:
             print(row['RTSP IP'],row['store'])
-            video=cv2.VideoCapture(row['RTSP IP'])
-                # print("tryyyyyyyyyyyyyy")
-            if video is None or not video.isOpened():
-                
+            try:
+                video=cv2.VideoCapture(row['RTSP IP'])
+                    # print("tryyyyyyyyyyyyyy")
+                # if video is None or not video.isOpened():
+            except:
             #    cv2.VideoCapture(row['RTSP URL'])
-                video1=cv2.VideoCapture(row['RTSP URL'])
-                if video1 is None or not video1.isOpened():
+                try:
+                    video1=cv2.VideoCapture(row['RTSP URL'])
+                
+                    # if video1 is None or not video1.isOpened():
+                except:
                     Rtps_url.append([row['RTSP IP'],row['store'],"Not working"])
-                         
     df2 = pd.DataFrame(Rtps_url)
     # print("DF2")
     # print(df2)
