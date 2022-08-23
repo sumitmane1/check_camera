7a4515ef0859167773e6bedc389367e4781c6079
sumitmane1 <108738719+sumitmane1@users.noreply.github.com>
2022-08-23 22:30:59 +0530

    Create camera.py

---
+++ camera.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import cv2
+df = pd.read_csv(r"C:\Users\Sumit\Desktop\CV RTSP Task\test.csv")
+Rtps_url = []
+Store = int(input("Enter Store Number:"))
+for i,row in df.iterrows():
+    if row['store']==Store:
+        print(row['RTSP IP'],row['store'])
+        try:
+            try:
+                video=cv2.VideoCapture(row['RTSP IP'])
+                # print("tryyyyyyyyyyyyyy")
+                if video is None or not video.isOpened():
+
+                    raise ConnectionError
+            # try:
+            #     cv2.VideoCapture(row['RTSP IP'])
+            #     print("tryyyyyyyyyyyyyy")
+                # Rtps_url.append([row['RTSP IP'],row['store']])
+            except ConnectionError:
+                # print("start")
+                cv2.VideoCapture(row['RTSP URL'])
+                # print("exceptttttttttttttttt")
+                if video is None or not video.isOpened():
+                    raise ConnectionError
+                # Rtps_url.append([row['RTSP IP'],row['store']])
+        except ConnectionError :
+            # print("ERRORr")
+            # print(e)
+            # print(Rtps_url)
+            Rtps_url.append([row['RTSP IP'],row['store']])            
+    df2 = pd.DataFrame(Rtps_url)
+    # print("DF2")
+    # print(df2)
+    df2.to_csv("Output.csv")
